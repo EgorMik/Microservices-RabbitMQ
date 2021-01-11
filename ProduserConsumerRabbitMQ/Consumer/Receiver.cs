7197cf7f6523d2d916d43571631100ba9fd52078
Egor <egormikulic@gmail.com>
2021-01-11 14:54:12 +0300

    fix solution

--- ProduserConsumerRabbitMQ/Consumer/Receiver.cs
+++ ProduserConsumerRabbitMQ/Consumer/Receiver.cs
@@ -18,9 +18,9 @@ namespace Consumer
                 var consumer = new EventingBasicConsumer(channel);
                 consumer.Received += (model, ea) =>
                 {
-                    var body = ea.Body.Span;
+                    var body = ea.Body.ToArray();
                     var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine("Recieved message {0...}", message);
+                    Console.WriteLine("[x] Recieved message {0}", message);
                 };
                 channel.BasicConsume("BasicTest", true, consumer);
                 Console.WriteLine("Press [enter] to exit the Consumer...");
